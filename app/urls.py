e1ab7e7458ba86663f6573f1012dcea61b0761a1
hojuly <hojune.choi.me@gmail.com>
2021-04-08 10:29:17 +0900

    add navbar

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,8 @@ from app import views
 app_name = "app"
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('', views.index, name="index"),
     path('upload/', views.upload, name="upload"),
     path('train/', views.train, name="train"),
+    path('predict/', views.predict, name="predict"),
 ]
