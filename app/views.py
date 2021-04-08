4281e03470feb34e9dfc6ed99b6de0ea147fb0b4
hojuly <hojune.choi.me@gmail.com>
2021-04-08 15:05:08 +0900

    [#2] predict implement
    - predict implement

--- app/views.py
+++ app/views.py
@@ -49,16 +49,17 @@ def train(request):
 
 
     FAST_RUN = False
-    IMAGE_WIDTH=128
-    IMAGE_HEIGHT=128
-    IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
-    IMAGE_CHANNELS=3
+    IMAGE_WIDTH = 128
+    IMAGE_HEIGHT = 128
+    IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+    IMAGE_CHANNELS = 3
 
     filenames = os.listdir("./data/")
     print("file num => ", len(filenames))
     print("-"*50)
     categories = []
-    for filename in filenames:   
+    for filename in filenames:
+        """TODO: Change not only dog/cat""" 
         category = filename.split('.')[0]
         if category == 'dog':
             categories.append(1)
@@ -100,15 +101,16 @@ def train(request):
     print("### MODEL READY ###")
     earlystop = EarlyStopping(patience=10)
 
-    learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+    learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', 
                                                 patience=2, 
                                                 verbose=1, 
                                                 factor=0.5, 
-                                                min_lr=0.00001)
+                                                min_lr=0.0001)
 
     callbacks = [earlystop, learning_rate_reduction]
 
     df["category"] = df["category"].replace({0: 'cat', 1: 'dog'})
+    
     print("### TRAIN TEST SPLIT ###")
     train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
     train_df = train_df.reset_index(drop=True)
@@ -149,8 +151,9 @@ def train(request):
         class_mode='categorical',
         batch_size=batch_size
     )
+
     print("### MODEL TRAIN ###")
-    epochs=3 if FAST_RUN else 5
+    epochs=3 if FAST_RUN else 100
     history = model.fit(
         train_generator,
         epochs=epochs,
@@ -159,6 +162,7 @@ def train(request):
         steps_per_epoch=total_train//batch_size,
         callbacks=callbacks
     )
+
     print("### MODEL SAVE ###")
     model.save_weights("./model/cnn_model.h5")
     
@@ -167,5 +171,61 @@ def train(request):
 
 
 def predict(request):
-    """TODO"""
-    pass
+    import numpy as np
+
+    from keras.preprocessing import image
+    from keras.models import load_model
+    from keras.models import Sequential
+    from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+
+
+    IMAGE_WIDTH = 128
+    IMAGE_HEIGHT = 128
+    IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+    IMAGE_CHANNELS = 3
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten()) 
+
+    model.add(Dense(512, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy']) 
+
+    model.summary() #log?
+
+    if request.method == 'POST' and request.FILES['test']:
+        test = request.FILES['test']
+        _handle_uploaded_file(test, "test.jpg")
+        
+        img = image.load_img("./data/test.jpg", target_size=(128, 128))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = img/255
+
+        model.load_weights('./model/cnn_model.h5')
+
+        guess = np.argmax(model.predict(img), axis=-1)
+        out = 'dog' if guess == 1 else 'cat'
+        context = {'what': [out, 'prob cat:', model.predict(img)[0][0], ' prob dog: ', model.predict(img)[0][1]]}
+    
+        return render(request, 'app/index.html', context)
+
+    return render(request, 'app/index.html', {'what': 'wrong access'})
